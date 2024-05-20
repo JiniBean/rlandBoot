fb062c54f8a69334964935d0a5eaf16b13497f0a
JiniBean_hi <JiniBean@DESKTOP-2BGN6PD>
2024-05-20 12:37:20 +0900

    채팅 프로그램 시작

---
+++ src/main/java/kr/co/rland/web/socket/config/ChatWebSocketHandler.java
@@ -0,0 +1,33 @@
+package kr.co.rland.web.socket.config;
+
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatWebSocketHandler extends TextWebSocketHandler{
+
+    List<WebSocketSession> users = new ArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        users.add(session);
+        System.out.println("connected from" + session.getRemoteAddress());
+//        session.sendMessage(new TextMessage("welcome!"));
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        users.remove(session);
+        System.out.println("closed from" + session.getRemoteAddress());
+    }
+}
---
+++ src/main/resources/static/js/nodejs/rollup/target/bundle.js
@@ -0,0 +1,6 @@
+function test1$1() {
+    console.log("hello");
+}function test1() {
+    console.log("hello2");
+}test1$1();
+test1();
